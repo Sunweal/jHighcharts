5d4ffccc0a2faf7587e5f1b73af71da41f9e8d60
qianmoQ <shicheng@ttxit.com>
2016-07-19 17:30:04 +0800

    增加字体样式文件枚举类

---
+++ src/main/java/com/ttxit/jhighcharts/enums/Style.java
@@ -0,0 +1,16 @@
+package com.ttxit.jhighcharts.enums;
+
+/**
+ * 字体样式
+ * 
+ * @author shicheng
+ *
+ */
+public enum Style {
+    
+    NORMAL, // 标准的字体样式
+    ITALIC, // 斜体的字体样式
+    OBLIQUE, // 倾斜的字体样式
+    INHERIT, // 从父元素继承字体样式
+    
+}
